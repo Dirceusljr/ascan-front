214f801c410d609c5b54e346876f330fe554de05
dirceusljr <dirceusljr@gmail.com>
2025-10-23 21:31:07 -0300

    Ajustes no header

--- src/app/components/todo-header/todo-header.component.ts
+++ src/app/components/todo-header/todo-header.component.ts
@@ -2,26 +2,31 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
-import {
-  MatDialog,
-  MatDialogModule,
-} from '@angular/material/dialog';
+import { MatSelectModule } from '@angular/material/select';
+
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { TodoAddComponent } from '../todo-add/todo-add.component';
 
 @Component({
   selector: 'ascan-todo-header',
   standalone: true,
-  imports: [CommonModule, MatButtonModule, MatIconModule, MatDialogModule],
+  imports: [
+    CommonModule,
+    MatButtonModule,
+    MatIconModule,
+    MatDialogModule,
+    MatSelectModule,
+  ],
   templateUrl: './todo-header.component.html',
-  styleUrl: './todo-header.component.css'
+  styleUrl: './todo-header.component.css',
 })
 export class TodoHeaderComponent {
-  private dialogService = inject(MatDialog)
+  private dialogService = inject(MatDialog);
 
   public handleOpenModal(): void {
-    this.dialogService.open(TodoAddNewEntryFormComponent, {
+    this.dialogService.open(TodoAddComponent, {
       width: '50vw',
       maxHeight: '80vh',
-    })
+    });
   }
-
 }
